bacd16e70ab8ced4c6e6a621c15f4283115cfdf9
Fuhu Xiao <fuhuxiao@nvidia.com>
2024-11-16 22:26:00 -0800

    Modify entry point to work with log dataset

--- src/suql/experimental/embedding.py
+++ src/suql/experimental/embedding.py
@@ -1,12 +1,12 @@
 from suql.faiss_embedding import MultipleEmbeddingStore
 
 embedding_store = MultipleEmbeddingStore()
-embedding_store.add(table_name="log_normal",
-                    primary_key_field_name="line_id",
+embedding_store.add(table_name="log_records",
+                    primary_key_field_name="p_key",
                     free_text_field_name="content",
                     db_name="postgres",
-                    user="select_user",
-                    password="select_user")
+                    user="creator_role",
+                    password="creator_role")
 
 host = "127.0.0.1"
 port = 8501
--- src/suql/experimental/entry_point.py
+++ src/suql/experimental/entry_point.py
@@ -6,9 +6,12 @@ import litellm
 # suql = "SELECT * FROM log_normal WHERE answer(content, 'Does the log mention any system crashes or failures?') = 'Yes'"
 # suql = "SELECT * FROM log_normal WHERE answer(content, 'Is there an error?') = 'Yes' or answer(content, 'Is the error serious?') = 'Yes' or answer(content, 'Is the error big?') = 'Yes'"
 # suql = "SELECT * FROM log_normal WHERE is_relevant(content, 'What is happening?') OR is_relevant(content, 'What was wrong with the system?') OR answer(content, 'Is this line of log interesting?') = 'Yes'"
-suql = "SELECT * FROM log_normal WHERE is_relevant(content, 'What was wrong with the system?')"
+# suql = "SELECT * FROM log_normal WHERE is_relevant(content, 'What was wrong with the system?')"
+# suql = "SELECT * FROM log_records LIMIT 5"
+# suql = "SELECT level, content FROM log_records WHERE answer(content, 'Does this line of log contain an HTTP code?') = 'Yes' LIMIT 3"
+suql = "SELECT line_id, level, content FROM log_records WHERE answer(content, 'Does this line of log contain an HTTP code?') = 'Yes' AND level = 'ERROR' LIMIT 3"
 
-table_w_ids = {"log_normal": "line_id"}
+table_w_ids = {"log_records": "p_key"}
 database = "postgres"
 # Try better: llm_model_name="gpt-4o-mini"
 answer = suql_execute(suql, table_w_ids, database)
